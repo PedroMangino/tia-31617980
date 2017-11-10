b8b10bf82a95d599bf9326f398d2689d2a2f8a70
Pedro Mangino <pedromanginosanches@hotmail.com>
2017-11-10 18:58:48 -0200

    Add files via upload

---
+++ Mangindia.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 31637213
+ */
+public class Mangindia {
+    public static void main(String[] args) {
+        System.out.println("Hello, Mangindia!");
+    }
+    
+}
